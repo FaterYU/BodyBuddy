88bed735506db0ee01c83a8bd240f4732b217bfa
LLLLLrf <1626226046@qq.com>
2023-12-01 16:58:07 +0800

    feat: 添加视频播放页

--- App.js
+++ App.js
@@ -1,76 +1,88 @@
 import * as React from 'react';
-import {Text, View, TouchableWithoutFeedback} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { NativeBaseProvider} from "native-base";
+import { NativeBaseProvider } from "native-base";
 import 'react-native-gesture-handler';
 import CommunityScreen from './src/screen/community';
 import CoursesScreen from './src/screen/courses';
 import PersonScreen from './src/screen/person';
 import CalendarScreen from './src/screen/calendar';
+import VideoScreen from './src/screen/video';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const MainTabs = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="CommunityScreen"
+      screenOptions={{
+        headerShown: false, // 隐藏标题栏
+        tabBarActiveTintColor: '#e91e63',
+        scrollEnabled: true,
+        animationEnabled: true,
+      }}
+    >
+      <Tab.Screen
+        name="Courses"
+        component={CoursesScreen}
+        options={({ route }) => ({
+          tabBarLabel: 'Courses',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="human" color={color} size={size} />
+          ),
+        })}
+      />
+      <Tab.Screen
+        name="Calendar"
+        component={CalendarScreen}
+        options={{
+          tabBarLabel: 'Calendar',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="calendar-month" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Community"
+        component={CommunityScreen}
+        options={{
+          tabBarLabel: 'Community',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home-city" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Person"
+        component={PersonScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function MyTabs() {
   return (
     <NativeBaseProvider>
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="CommunityScreen"
-        screenOptions={{
-          headerShown: false, // 隐藏标题栏
-          tabBarActiveTintColor: '#e91e63',
-          scrollEnabled: true,
-          animationEnabled: true,
-
-        }}
-      >
-        <Tab.Screen
-          name="Courses"
-          component={CoursesScreen}
-          options={({ route }) => ({
-            tabBarLabel: 'Courses',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="human" color={color} size={size} />
-            ),
-            })}
-        />
-        <Tab.Screen
-          name="Calendar"
-          component={CalendarScreen}
-          options={{
-            tabBarLabel: 'Calendar',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="calendar-month" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Community"
-          component={CommunityScreen}
-          options={{
-            tabBarLabel: 'Community',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="home-city" color={color} size={size} />
-
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Person"
-          component={PersonScreen}
-          options={{
-            tabBarLabel: 'Profile',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="account" color={color} size={size} />
-            ),
-          }}
-        />
-
-      </Tab.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="MainTabs"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="MainTabs" component={MainTabs} />
+          <Stack.Screen name="VideoScreen" component={VideoScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
---
+++ src/screen/video.js
@@ -0,0 +1,85 @@
+import React, { useEffect } from 'react';
+import {Text, View, Dimensions, StyleSheet, ScrollView, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback} from 'react-native';
+import Orientation from 'react-native-orientation-locker';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const VideoScreen = ({navigation}) => {
+    useEffect(() => {
+        // 设置为横屏
+        Orientation.lockToLandscape();
+    
+        // 在组件卸载时重置为竖屏
+        return () => {
+          Orientation.lockToPortrait();
+        };
+      }, []); // 确保这个 effect 只在组件挂载和卸载时执行
+      const goBack = () => {
+        navigation.navigate('Courses');
+    }
+    return (
+        <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start', flexDirection: 'row' }}>
+            <View style={styles.video}>
+                <TouchableOpacity onPressOut={goBack}>
+                    <MaterialCommunityIcons name="chevron-left-circle" size={26} color="rgba(200,200,200,0.8)" style={{alignSelf:'flex-start',marginTop:14,marginLeft:14}} />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.sideCard}>
+                <View style={styles.camera}>
+                    <Text style={{color:'black'}}>用户摄像头界面</Text>
+                </View>
+                <View style={styles.score}>
+                    <Text style={{color:'white',marginTop:30}}>当前评分</Text>
+                    <Text style={{color:'white',fontSize:44,fontWeight:700}}>93</Text>
+                    <Text style={{color:'white',marginBottom:30}}>继续保持</Text>
+                </View>
+            </View>
+
+
+            {/* <Video 
+                ref={ref = this.Video = ref}
+                source={{uri: "url"}}  
+                poster={"url"}
+                paused={paused}
+                onProgress={({currentTime}) => {}}
+                onLoad={({duration}) => {}}
+                onEnd={() => {}}
+                resizeMode="cover"
+                posterResizeMode="cover"
+                style={style}
+            /> */}
+        </View>
+    );
+    }
+
+const styles = StyleSheet.create({
+    video: {
+        height: '100%',
+        backgroundColor: 'gray',
+        width: '80%',
+    },
+    camera:{
+        height: '50%',
+        backgroundColor: 'rgba(200,200,200,0.8)',
+        width: '100%',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    sideCard:{
+        height: '100%',
+        width: '20%',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+    },
+    score:{
+        backgroundColor:'rgba(70,210,90,1)',
+        width: '100%',
+        height: '50%',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    }
+});
+
+export default VideoScreen;
