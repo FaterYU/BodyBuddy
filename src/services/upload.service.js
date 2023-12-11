e9433c63bc0a3281885487792fbddda63e11ccfc
FaterYU <1042277145@qq.com>
2023-12-12 02:37:10 +0800

    feat: file service

--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -1,10 +1,35 @@
 import http from '../http.js';
 class UploadFilesService {
-  upload(file, onUploadProgress, type, uploader) {
+  /**
+   * @param {File} file
+   * @param {Function} onUploadProgress
+   * @returns {Promise}
+   * @description
+   * 上传文件, onUploadProgress为上传进度回调函数, 用于更新进度条, 可以不传
+   * @example
+   * var progressInfos = { percentage: 0, fileName: file.name };
+      UploadService.upload(
+        file,
+        (event) => {
+          progressInfos.percentage = Math.round(
+            (100 * event.loaded) / event.total
+          );
+        },
+      )
+        .then((response) => {
+          if (response.data.message.includes("文件上传成功:")) {
+            console.log(response.data.message);
+          }
+        })
+        .catch(() => {
+          console.log('Upload Error');
+        });
+   */
+  upload(file, onUploadProgress) {
     let formData = new FormData();
     formData.append('file', file);
-    formData.append('type', type);
-    formData.append('uploader', uploader);
+    // formData.append('type', type);
+    // formData.append('uploader', uploader);
     return http.post('/upload', formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
@@ -12,12 +37,15 @@ class UploadFilesService {
       onUploadProgress,
     });
   }
-  getFiles() {
-    return http.get('/fileslist');
-  }
+  /**
+   *
+   * @param {JSON} data
+   * data:{name:'filename'}
+   * @returns {Promise}
+   */
   downloadFile(data) {
-    // data:{name:'filename',uploader:'uploader',type:'file's type'}
-    return http.post('/files', data, {responseType: 'blob'});
+    // data:{name:'filename'}
+    return http.post('/download', data, {responseType: 'blob'});
   }
 }
 export default new UploadFilesService();
