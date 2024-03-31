7fa91a5305cee52b0c7ca450f04828f3884e2276
LLLLLrf <1626226046@qq.com>
2024-04-01 01:05:16 +0800

    feat: 训练页基本完成

--- src/screen/vertical_video.js
+++ src/screen/vertical_video.js
@@ -46,16 +46,10 @@ const VideoScreen = ({ navigation, route }) => {
   const [isVisible, setIsVisible] = useState(false);
   const [CurrentScore, setCurrentScore] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
-  const [score, setScore] = useState([]);
+  const [duration, setDuration] = useState(0);
 
-  const level = ['Try Harder!', 'Keep Doing!', 'Good Job!', 'Excellent!'];
-  const levelColor = [
-    'rgba(220,30,30,0.8)',
-    'rgba(220,220,30,0.8)',
-    'rgba(30,220,30,0.8)',
-    'rgba(30,220,220,0.8)',
-  ];
-  const scores=[90, 67, 93, 83, 81, 89, 62, 55, 85, 51, 53, 54, 87, 61, 60, 77, 79, 83, 88, 84, 75, 95, 97, 91, 92, 87, 89, 88, 79, 88, 89, 91, 81, 76, 86, 82, 85, 81, 82, 87, 90];
+  const [averageScore, setAverageScore] = useState(0);
+  const scores = [90, 67, 93, 83, 81, 89, 62, 55, 85, 51, 53, 54, 87, 61, 60, 77, 79, 83, 88, 84, 75, 95, 97, 91, 92, 87, 89, 88, 79, 88, 89, 91, 81, 76, 86, 82, 85, 81, 82, 87, 90];
 
   const handleProgress = (data) => {
     // 获取视频当前播放时间
@@ -76,6 +70,7 @@ const VideoScreen = ({ navigation, route }) => {
       setIsVisible(false);
     }, 3000);
   };
+
   const finishCourse = () => {
     FitsService.getOneFitScoreById({
       id: fitId,
@@ -93,6 +88,13 @@ const VideoScreen = ({ navigation, route }) => {
     const currentSecond = Math.floor(currentTime);
     if (scores.length > 0 && currentSecond < scores.length) {
       setCurrentScore(scores[currentSecond]);
+      console.log('current score:', scores[currentSecond])
+      // set average score
+      let sum = 0;
+      for (let i = 0; i < scores.length; i++) {
+        sum += scores[i];
+      }
+      setAverageScore(Math.round(sum / scores.length));
     }
   }, [currentTime, scores]);
 
@@ -113,7 +115,7 @@ const VideoScreen = ({ navigation, route }) => {
             <MaterialCommunityIcons
               name="chevron-left-circle"
               size={26}
-              color="rgba(180,180,180,0.8)"
+              color="rgba(190,190,190,0.8)"
               style={{ alignSelf: 'flex-start' }}
             />
           </TouchableOpacity>
@@ -141,10 +143,12 @@ const VideoScreen = ({ navigation, route }) => {
             }}
             // onPlay={handleVideoPlay}
             style={{ width: screenWidth, height: screenHeight * 0.36 }}
-            controls={true}
+            // 暂时隐藏控制条，控制条样式有Bug
+            controls={false}
             resizeMode="cover"
             paused={!playVideos}
             onProgress={handleProgress}
+
             onEnd={finishCourse}
           />
           <View
@@ -159,12 +163,13 @@ const VideoScreen = ({ navigation, route }) => {
               // onPlay={handleVideoPlay}
               style={{ width: screenWidth * 0.7, height: screenHeight * 0.64 }}
               resizeMode="cover"
-              onProgress={handleProgress}
+              // onProgress={handleProgress}
+              onEnd={finishCourse}
               paused={!playVideos}
             />
             <VerticalGrid
               value={CurrentScore}
-
+              avg={averageScore}
             />
           </View>
         </View>
@@ -192,21 +197,21 @@ const VideoScreen = ({ navigation, route }) => {
 };
 
 
-const VerticalGrid = ({ value }) => {
+const VerticalGrid = ({ value, avg }) => {
   const [gridColors, setGridColors] = useState(Array(20).fill('#FFFFFF'));
-  const val=value;
-  console.log(value);
+  const val = value;
+
   useEffect(() => {
     const updatedGridColors = gridColors.map((color, index) => {
-      if (100-(index * 5) >= value) {
+      if (100 - (index * 5) > value) {
         return '#FFFFFF';
-      }else if(value>=90){
+      } else if (value >= 90) {
         return 'rgba(255,0,0,1)';
-      }else if(value>=80){
+      } else if (value >= 80) {
         return 'rgba(255,165,0,1)';
-      }else if(value>=70){
+      } else if (value >= 70) {
         return 'rgba(255,255,0,1)';
-      }else if(value>=60){
+      } else if (value >= 60) {
         return 'rgba(0,128,0,1)';
       }
     });
@@ -217,26 +222,53 @@ const VerticalGrid = ({ value }) => {
     <View style={{
       flexDirection: 'column',
       justifyContent: 'space-around',
-      height: (screenHeight*0.64),
-      }}>
+      height: (screenHeight * 0.64),
+    }}>
+      <View style={{
+        position:'absolute',
+        top: Math.round(screenHeight * 0.64/20)*2,
+        height:2,
+        width:screenWidth*0.3,
+        backgroundColor:'rgba(25,25,25,1)',
+        zIndex: 999,
+      }}></View>
+      {/*
+      <View style={{
+        position:'absolute',
+        bottom: ((screenHeight * 0.64/100)*avg),
+        height:2,
+        width:screenWidth*0.3,
+        backgroundColor:'rgba(25,25,25,1)',
+        zIndex: 998,
+      }}></View> */}
+
+
       <View
         style={{
-          position:'absolute',
-          height:screenHeight*0.64,
-          width:screenWidth*0.3,
+          position: 'absolute',
+          height: screenHeight * 0.64,
+          width: screenWidth * 0.3,
           top: 0,
-          left:0,
-          zIndex:1000,
-          justifyContent:'center',
-          alignItems:'center',
+          left: 0,
+          zIndex: 1000,
+          justifyContent: 'space-around',
+          alignItems: 'center',
         }}
       >
-        <Text style={{fontSize:36,color:'white',fontWeight:'bold'}}>{val}</Text>
+        <Text style={{
+          fontSize: 36,
+          color: 'white',
+          fontWeight: 'bold',
+          textShadowOffset: { width: 0, height: 0 },
+          textShadowRadius: 6,
+          textShadowColor: 'rgba(0,0,0,1)',
+        }}>{val}</Text>
       </View>
       {gridColors.map((color, index) => (
-        <View key={index} style={{ width: screenWidth*0.3, height: (screenHeight*0.64)/20, backgroundColor: color,
-          borderBottomWidth: 2, borderBottomColor: 'rgba(30,30,30,0.8)', borderStyle: 'solid'
-      }} />
+        <View key={index} style={{
+          width: screenWidth * 0.3, height: Math.round((screenHeight * 0.64) / 20)-1, backgroundColor: color,
+          borderBottomWidth: 2, borderBottomColor: 'rgba(256,256,256,0)', borderStyle: 'solid'
+        }} />
       ))}
     </View>
   );
